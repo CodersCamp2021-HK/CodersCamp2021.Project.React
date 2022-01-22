7eb5a89a3e456eb7449d8a26bf60d3327ace697b
tchojnacki <tomaszchojnacki2001@gmail.com>
2022-01-22 12:51:54 +0100

    feat(ui): implement basic LevelSelectPage layout

--- src/shared/theme/Theme.js
+++ src/shared/theme/Theme.js
@@ -2,8 +2,16 @@ import { transformToCssVariablesTheme } from './Utils';
 
 const valueTheme = {
   colors: {
-    background: '#282c34',
-    link: '#61dafb',
+    common: {
+      white: '#ffffff',
+    },
+    primary: {
+      main: '#facd74',
+    },
+    background: {
+      solid: '#3f3851',
+      transparent: 'rgba(0, 0, 0, 0.65)',
+    },
   },
 };
 
