2c857184366d17e5cd792e259972cb70a4104f1d
Hubert Kawałek <hubert.kawalek.dev@gmail.com>
2022-01-26 22:44:15 +0100

    build: use target:'esnext' to prevent transpilation of private members

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    target: 'esnext',
+    minify: false,
+  },
   plugins: [
     react({
       jsxImportSource: '@emotion/react',
