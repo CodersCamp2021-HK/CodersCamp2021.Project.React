7d609aa10914005fb4d58cf08cecfcb9f8a3dabc
Dudek Kamil <dudekkam@codelab.eu>
2022-01-26 16:30:39 +0100

    styles: display game in border

--- src/shared/ui/UIProxy.js
+++ src/shared/ui/UIProxy.js
@@ -1,22 +1,12 @@
 class UIProxy {
-  #setDistanceCb;
-
   #setLoseCb;
 
   /**
-   * @param {(distance: number) => void} setDistanceCb
+
    * @param {() => void} setLoseCb
    */
-  constructor(setDistanceCb, setLoseCb) {
-    this.#setDistanceCb = setDistanceCb;
+  constructor(setLoseCb) {
     this.#setLoseCb = setLoseCb;
-  }
-
-  /**
-   * @param {number} distance
-   */
-  setDistance(distance) {
-    this.#setDistanceCb(distance);
   }
 
   setLose() {
--- src/ui/pages/GameUI.jsx
+++ src/ui/pages/GameUI.jsx
@@ -1,10 +1,35 @@
 import { css } from '@emotion/react';
 import { useLayoutEffect, useMemo, useRef, useState } from 'react';
-import { useGameEngine, UIProxy } from '../../shared';
+import { useGameEngine, UIProxy, theme } from '../../shared';
+import backgroundUrl from '../../public/img/background.jpg';
+import { PageHeader } from '../components';
+
+const wrapper = css`
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  background: ${theme.colors.background.solid} url(${backgroundUrl}) center center / cover;
+`;
+
+const gameBorder = css`
+  width: 100%;
+  max-width: max(min(50vw, 1200px), 1200px);
+  max-height: 1000vh;
+  overflow-y: auto;
+
+  border: 1rem solid ${theme.colors.primary.main};
+  display: grid;
+
+  place-items: center;
+  background: ${theme.colors.background.transparent};
+`;
 
 const btn = css({
   border: '1px black solid',
-  padding: '5px 15px',
+  marginBottom: '30px',
   '&:hover': {
     background: '#e1e1e1',
   },
@@ -14,7 +39,6 @@ const GameUI = () => {
   const gameEngine = useGameEngine();
   const [running, setRunning] = useState(false);
   const [lose, setLose] = useState(false);
-  const [distance, setDistance] = useState(0);
 
   /** @type {React.MutableRefObject<HTMLCanvasElement | null>} */
   const ref = useRef(null);
@@ -23,19 +47,15 @@ const GameUI = () => {
     gameEngine.reset();
     setRunning(false);
     setLose(false);
-    setDistance(0);
   };
 
   const uiProxy = useMemo(
     () =>
-      new UIProxy(
-        (val) => setDistance(val),
-        () => {
-          setLose(true);
-          gameEngine.stop();
-          setRunning(false);
-        },
-      ),
+      new UIProxy(() => {
+        setLose(true);
+        gameEngine.stop();
+        setRunning(false);
+      }),
     [gameEngine],
   );
 
@@ -46,46 +66,47 @@ const GameUI = () => {
   }, [gameEngine, uiProxy]);
 
   return (
-    <div css={{ display: 'flex', flexDirection: 'column', gap: '10px', margin: '20px', height: '300px' }}>
-      <div css={{ display: 'flex', gap: '10px', alignItems: 'center' }}>
-        {running ? (
-          <button
-            css={btn}
-            type='button'
-            onClick={() => {
-              gameEngine.stop();
-              setRunning(false);
-            }}
-          >
-            Stop
-          </button>
-        ) : (
-          <button
-            css={btn}
-            type='button'
-            onClick={() => {
-              if (lose) {
-                reset();
-              }
-              gameEngine.start();
-              setRunning(true);
-            }}
-          >
-            Start
-          </button>
-        )}
-        <button css={btn} type='button' onClick={reset}>
-          Reset
+    <div css={wrapper}>
+      <PageHeader>Level 1</PageHeader>{' '}
+      {running ? (
+        <button
+          css={btn}
+          type='button'
+          onClick={() => {
+            gameEngine.stop();
+            setRunning(false);
+          }}
+        >
+          Stop
         </button>
-        <span>Distance: {distance}</span>
-      </div>
+      ) : (
+        <button
+          css={btn}
+          type='button'
+          onClick={() => {
+            if (lose) {
+              reset();
+            }
+            gameEngine.start();
+            setRunning(true);
+          }}
+        >
+          Start
+        </button>
+      )}
+      <button css={btn} type='button' onClick={reset}>
+        Reset
+      </button>
       {lose && (
         <span css={{ fontSize: '35px', textTransform: 'uppercase', position: 'absolute', left: '190px', top: '110px' }}>
           You Lose
         </span>
       )}
-      <canvas css={{ width: '512px', height: '512px' }} height={512} width={512} id='GameCanvas' ref={ref} />
+      <div css={gameBorder}>
+        <canvas css={{ width: '1150px', height: '512px' }} height={512} width={512} id='GameCanvas' ref={ref} />
+      </div>
     </div>
+    // </div>
   );
 };
 
