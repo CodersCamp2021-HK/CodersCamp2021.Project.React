d6176ec27fcf88df2ef45248899b257b38dddcdd
mmejer <44590421+mmejer@users.noreply.github.com>
2022-01-26 11:05:48 +0100

    fix: eslint error

--- src/game/objects/index.js
+++ src/game/objects/index.js
@@ -3,6 +3,7 @@ export * from './DistanceUpdater';
 export * from './ObstaclesGenerator';
 export * from './Trex';
 export * from './TrexStatefull';
+// eslint-disable-next-line import/no-cycle
 export * from './KingStatefull';
 export * from './SolidTile';
 export * from './BackgroundTiles';
