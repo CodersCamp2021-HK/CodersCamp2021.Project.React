42f507880730d5655bf489851afd6f2e45b9151e
mmejer <44590421+mmejer@users.noreply.github.com>
2022-01-28 22:55:46 +0100

    feat: add 'go back to homepage' button

--- src/ui/pages/CreditsPage/CreditsPage.jsx
+++ src/ui/pages/CreditsPage/CreditsPage.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { css } from '@emotion/react';
 import { theme } from '../../../shared/theme';
-import { PageHeader, Button } from '../../components';
+import { PageHeader, Button, BUTTON_WIDTH_SIZE } from '../../components';
 import backgroundUrl from '../../../public/img/background.jpg';
 
 const wrapper = css`
@@ -15,21 +15,42 @@ const wrapper = css`
   background: ${theme.colors.background.solid} url(${backgroundUrl}) center center / cover;
 `;
 
-const credits = css`
+const creditsWrapper = css`
+  position: relative;
   width: 100%;
   max-width: max(min(50vw, 1000px), 500px);
   max-height: 50vh;
+`;
+
+const credits = css`
   overflow-y: auto;
   border: 1rem solid ${theme.colors.primary.main};
   background: ${theme.colors.background.transparent};
-  z-index: 100;
+`;
+
+const buttonWrapper = css`
+  position: absolute;
+  width: ${BUTTON_WIDTH_SIZE};
+  margin-left: auto;
+  margin-right: auto;
+  bottom: -${4.8 / 2}rem;
+  left: 0;
+  right: 0;
 `;
 
 const CreditsPage = () => {
   return (
     <main css={wrapper}>
       <PageHeader>Credits</PageHeader>
-      <section css={credits} />
+      <div css={creditsWrapper}>
+        <section css={credits}>
+          <div css={buttonWrapper}>
+            <Button type='silver' onClick={() => {}}>
+              homepage
+            </Button>
+          </div>
+        </section>
+      </div>
     </main>
   );
 };
