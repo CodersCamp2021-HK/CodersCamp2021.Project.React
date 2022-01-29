dff02586c9f1f440edac282d834d26983d9f1166
Marta Mejer <44590421+mmejer@users.noreply.github.com>
2022-01-29 23:05:18 +0100

    fix: remove unused import

--- src/ui/pages/CreditsPage/CreditsPage.jsx
+++ src/ui/pages/CreditsPage/CreditsPage.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { css } from '@emotion/react';
 import { theme } from '../../../shared/theme';
 import { PageHeader, Button, BUTTON_WIDTH_SIZE } from '../../components';
