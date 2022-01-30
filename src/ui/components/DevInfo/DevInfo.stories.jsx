2ce3ef704893386c4ccf180abba5d4bcacf7746c
mmejer <44590421+mmejer@users.noreply.github.com>
2022-01-30 02:39:07 +0100

    stories: update DevInfo stories

--- src/ui/components/DevInfo/DevInfo.stories.jsx
+++ src/ui/components/DevInfo/DevInfo.stories.jsx
@@ -4,12 +4,26 @@ import imgUrl from '../../../public/img/tc_pixel.png';
 export default {
   title: 'Components/Dev Info',
   component: DevInfoComponent,
+  special: {
+    options: ['true', 'false'],
+  },
 };
 
 /** @type {typeof DevInfoComponent & Partial<{ args: object }>} */
-export const DevInfo = ({ image, name, devRole }) => <DevInfoComponent image={image} name={name} devRole={devRole} />;
-DevInfo.args = {
+const Template = DevInfoComponent;
+
+export const Dev = Template.bind({});
+Dev.args = {
   image: imgUrl,
   name: 'Jan Kowalski',
   devRole: 'Project Manager, Programmer',
+  special: false,
+};
+
+export const Mentor = Template.bind({});
+Mentor.args = {
+  image: imgUrl,
+  name: 'Jan Kowalski',
+  devRole: 'Project Manager, Programmer',
+  special: true,
 };
