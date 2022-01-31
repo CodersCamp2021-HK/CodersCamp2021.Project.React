579ed6fa912267b92d9c75b54d5a506ffa92adf9
Marta Mejer <44590421+mmejer@users.noreply.github.com>
2022-01-31 18:27:15 +0100

    Delete unnecessary files (#59)

--- src/game/objects/index.js
+++ src/game/objects/index.js
@@ -1,8 +1,3 @@
-export * from './Background';
-export * from './DistanceUpdater';
-export * from './ObstaclesGenerator';
-export * from './Trex';
-export * from './TrexStatefull';
 // eslint-disable-next-line import/no-cycle
 export * from './King';
 // eslint-disable-next-line import/no-cycle
