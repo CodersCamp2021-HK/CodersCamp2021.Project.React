7125844060b21a331c2502665fd16616c24eb527
Kamil <37578394+KamilDudek@users.noreply.github.com>
2022-02-04 11:07:50 +0100

    Issue 58 local storage czy poziom odblokowany (#72)
    * feat: change onSelect to hover
    
    * feat: add template levels
    
    * feat: add localStorage, check if level is unlocked
    
    * refactor: add types
    
    * fix: delete second levelNumber
    
    * feat: block go to level in url
    
    * refactor: delete unnecessary imports
    
    * refactor: change levelButton stories
    
    * refactor: change stories and test
    
    * refactor: change return condition
    
    * refactor: change fun name
    
    Co-authored-by: Dudek Kamil <dudekkam@codelab.eu>

--- src/ui/components/LevelButton/LevelButton.test.jsx
+++ src/ui/components/LevelButton/LevelButton.test.jsx
@@ -3,37 +3,15 @@ import { LevelButton } from './LevelButton';
 
 describe('LevelButton', () => {
   it('should render the button', () => {
-    render(
-      <LevelButton type='selected' onSelect={() => {}}>
-        1
-      </LevelButton>,
-    );
+    render(<LevelButton type='unlocked'>1</LevelButton>);
 
     const button = screen.getByRole('button', { name: '1' });
     expect(button).toBeInTheDocument();
   });
 
-  it('can be clicked when unlocked', () => {
-    const onSelect = jest.fn();
-    render(
-      <LevelButton type='unlocked' onSelect={onSelect}>
-        2
-      </LevelButton>,
-    );
-
-    const button = screen.getByRole('button', { name: '2' });
-    fireEvent.click(button);
-
-    expect(onSelect).toHaveBeenCalledTimes(1);
-  });
-
   it('should be disabled when locked', () => {
     const onSelect = jest.fn();
-    render(
-      <LevelButton type='locked' onSelect={onSelect}>
-        3
-      </LevelButton>,
-    );
+    render(<LevelButton type='locked'>3</LevelButton>);
 
     const button = screen.getByRole('button', { name: '3' });
     fireEvent.click(button);
