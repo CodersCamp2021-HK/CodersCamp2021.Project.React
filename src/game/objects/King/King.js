6d5dec4277f6ef9f69e382691c043a3b2d7b1c2c
tchojnacki <tomaszchojnacki2001@gmail.com>
2022-02-04 20:00:44 +0100

    refactor(game): remove setTimeout from king attack

--- src/game/objects/King/King.js
+++ src/game/objects/King/King.js
@@ -1,6 +1,6 @@
 import { Vector } from '../../shared';
 import { GameObject, BoxCollider } from '../../engine';
-import { KING_ATTACK_DELAY } from '../../config';
+import { KING_ATTACK_DELAY_FRAMES } from '../../config';
 import { KingAttack } from './KingAttack';
 import { KingDead } from './KingDead';
 import { KingDoorIn } from './KingDoorIn';
@@ -27,21 +27,18 @@ class King extends GameObject {
 
   #isOnGround = false;
 
-  #canAttack = true;
+  #attackCooldownFrames = 0;
 
   get isOnGround() {
     return this.#isOnGround;
   }
 
   get canAttack() {
-    return this.#canAttack;
+    return this.#attackCooldownFrames <= 0;
   }
 
   delayAttack() {
-    this.#canAttack = false;
-    setTimeout(() => {
-      this.#canAttack = true;
-    }, KING_ATTACK_DELAY);
+    this.#attackCooldownFrames = KING_ATTACK_DELAY_FRAMES;
   }
 
   flipRight() {
@@ -76,6 +73,10 @@ class King extends GameObject {
   onUpdate(frame) {
     this.#state?.update(frame);
     this.#isOnGround = false;
+
+    if (this.#attackCooldownFrames > 0) {
+      this.#attackCooldownFrames -= 1;
+    }
   }
 
   /**
