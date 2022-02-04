79fb2f4ad928650fd81c87a43cb4798dc9d2485c
jskrajna <jskrajna@gmail.com>
2022-02-04 15:05:57 +0100

    przedrostek do routingu (#73)
    * test idea
    
    * check enironment and put static foldername
    
    * fix lint
    
    * final way to resolve this problem
    
    * remove global variable and add only basename
    
    Co-authored-by: Justyna Skrajna <justynaskrajna@Justynas-MacBook-Pro.local>

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import process from 'process';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { GameEngineProvider, GlobalStyles } from './shared';
 import { GameUI, LevelSelectPage, CreditsPage, HomePage, NotFound } from './ui';
 import './public/index.css';
 
+const githubPrefix = process.env.NODE_ENV === 'production' ? '/CodersCamp2021.Project.React' : '/';
+
 const App = () => {
   return (
     <>
       <GlobalStyles />
-      <BrowserRouter>
+      <BrowserRouter basename={githubPrefix}>
         <Routes>
           <Route path='/' element={<HomePage />} />
           <Route path='/level-select' element={<LevelSelectPage />} />
@@ -21,7 +24,7 @@ const App = () => {
               </GameEngineProvider>
             }
           />
-          <Route path='credits' element={<CreditsPage />} />
+          <Route path='/credits' element={<CreditsPage />} />
           <Route path='*' element={<NotFound />} />
         </Routes>
       </BrowserRouter>
