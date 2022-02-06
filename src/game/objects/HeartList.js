df81f6041811a5068d7a1f83543441ebe8ee97d0
mmejer <44590421+mmejer@users.noreply.github.com>
2022-02-06 17:28:26 +0100

    Refactor 'HeartList.js'

--- src/game/objects/HeartList.js
+++ src/game/objects/HeartList.js
@@ -4,26 +4,15 @@ import { GameObject } from '../engine';
 
 class HeartList extends GameObject {
   /**
-   * @type {number}
-   */
-  #heartsNumber;
-
-  /**
    * @param {number} hp
    */
   createHeartList(hp) {
     console.log(`create list with ${hp} hearts`);
-    this.#heartsNumber = hp;
     let start = 0;
     for (let i = 0; i < hp; i++) {
       this.create(Heart, { args: { initialPos: new Vector(start, 0) } });
       start += 15;
     }
-  }
-
-  onUpdate() {
-    this.destroy(this);
-    this.createHeartList(this.#heartsNumber - 1);
   }
 }
 
